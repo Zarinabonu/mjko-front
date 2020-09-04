e5b98a85103fb633c4c15eecf9b154888ec8b50c
Zarinabonu <zarinabonu199924@example.com>
2020-09-04 18:08:14 +0500

    first commit

---
+++ static/main.js
@@ -0,0 +1,12 @@
+Vue.component('news-list', {
+    template: `
+    <p>Имам Тирмизи стал международным исследовательским центром</p>
+    <div class="news-date-views">
+        <p>11 Сентября, 2012 12:49</p>
+        <p>
+            <img src="../static/img/logo-views.svg" alt="count views">
+            851
+        </p>
+    </div>
+`
+})
